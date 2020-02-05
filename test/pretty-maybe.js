189ab7f9a69aeda51700fb0ce5ba2cdb488988fe
Andreas Lind <andreaslindpetersen@gmail.com>
2020-02-06 00:33:37 +0100

    Give up on testing require.main.require('prettier') throwing

--- test/pretty-maybe.js
+++ test/pretty-maybe.js
@@ -4,15 +4,14 @@ const { mkdir, writeFile, readFile } = require('fs').promises;
 const pathModule = require('path');
 const { promisify } = require('util');
 const rimraf = promisify(require('rimraf'));
-const proxyquire = require('proxyquire');
 
-describe('maybe-pretty', function() {
+describe('pretty-maybe', function() {
   let tmpDir;
   let nextTmpDirNumber = 1;
   beforeEach(async () => {
     tmpDir = pathModule.resolve(
       require('os').tmpdir(),
-      `maybe-pretty-test-${Date.now()}-${process.pid}-${nextTmpDirNumber++}`
+      `pretty-maybe-test-${Date.now()}-${process.pid}-${nextTmpDirNumber++}`
     );
     await mkdir(tmpDir);
   });
@@ -89,18 +88,6 @@ describe('maybe-pretty', function() {
     });
   });
 
-  describe('when prettier is unavailable', function() {
-    const prettyMaybe = proxyquire('../', {
-      prettier: null
-    });
-
-    it('should write the data unchanged', async function() {
-      const fileName = pathModule.resolve(tmpDir, 'myFile.js');
-      const code = await prettyMaybe(fileName, 'a="b"');
-      expect(code, 'to equal', 'a="b"');
-    });
-  });
-
   describe('sync', function() {
     describe('when prettier is available', function() {
       describe('with config', function() {
@@ -137,14 +124,6 @@ describe('maybe-pretty', function() {
             expect(code, 'to equal', "a = 'b';\n");
           });
         });
-      });
-    });
-
-    describe('when prettier is unavailable', function() {
-      it('should write the data unchanged', async function() {
-        const fileName = pathModule.resolve(tmpDir, 'myFile.js');
-        const code = prettyMaybe.sync(fileName, 'a="b"');
-        expect(code, 'to equal', 'a="b"');
       });
 
       describe('without config', function() {
